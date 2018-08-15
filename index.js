64099333bad4eb25fa30e4a21a8f90ab18123f1a
vanaddo@gmail.com <vanaddo@gmail.com>
2018-08-15 01:18:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function happyHolidaysTo(holiday,name)
   return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountdown(days,holiday)
+function holidayCountdown(holiday,holiday)
 {
   return `It's ${days} days until ${holiday}!`
 }
