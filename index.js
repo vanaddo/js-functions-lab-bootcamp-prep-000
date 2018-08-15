b8d5b45177fe61be431570d619a5e565cf55da33
vanaddo@gmail.com <vanaddo@gmail.com>
2018-08-15 01:21:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function happyHolidaysTo(name)
 
 function happyHolidayTo(holiday, name)
 {
-  return `Happy holidays, ${name}!`
+  return `Happy ${holiday}, ${name}!`
 }
 
 function holidayCountdown(holiday,days)
